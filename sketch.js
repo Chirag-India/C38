528ea74ec4249ee29014b66f444af76c97ce6c4c
Harish C <bine0066@kmr1.local>
2020-04-26 13:00:44 +0530

    jhjad

--- sketch.js
+++ sketch.js
@@ -8,8 +8,16 @@ var database;
 
 var form, player, game;
 
-var cars, car1, car2, car3, car4;
+var cars, car1, car2, car3, car4,car1_i,car2_i,car3_i,car4_i,ground,track;
+function preload(){
+  car1_i = loadImage("images/car1.png");
+  car2_i = loadImage("images/car2.png");
+  car3_i = loadImage("images/car3.png");
+  car4_i = loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
+  track = loadImage ("images/track.png");
 
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +35,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.End();
   }
 }
